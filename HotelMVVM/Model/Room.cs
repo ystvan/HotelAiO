aad61f60379a6a20f88d2a8e8bced000fe10b311
ystvan <istv0050@edu.easj.dk>
2016-04-06 10:06:42 +0200

    room starting

---
+++ HotelMVVM/Model/Room.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelMVVM.Model
+{
+    class Room
+    {
+        public int Room_No { get; set; }
+        public int Hotel_No { get; set; }
+        public string Room_Type { get; set; }
+        public string Room_Price { get; set; }
+
+        public Room(int roomNo, int hotelNo, string roomType, string roomPrice)
+        {
+            Room_No = roomNo;
+            Hotel_No = hotelNo;
+            Room_Type = roomType;
+            Room_Price = roomPrice;
+        }
+
+        public Room()
+        {
+
+        }
+        public override string ToString()
+        {
+            return string.Format("RoomNo: {0}, \nIn which hotel: {1},\nType: {2}\nPrice per night:", Room_No, Hotel_No, Room_Type, Room_Price);
+        }
+    }
+}
---
+++ HotelMVVM/Model/RoomCatalogSingleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelMVVM.Model
+{
+    //class RoomCatalogSingleton
+    //{
+    //    private static RoomCatalogSingleton instance = new RoomCatalogSingleton();
+
+    //    public static RoomCatalogSingleton Instance
+    //    {
+    //        get { return instance; }
+    //    }
+
+    //    public ObservableCollection<Room> Rooms { get; set; }
+
+    //    private HotelCatalogSingleton()
+    //    {
+    //        Hotels = new ObservableCollection<Hotel>();
+
+    //        Hotels = new ObservableCollection<Hotel>(new PersistenceFacade().GetHotels());
+    //    }
+
+    //    public void Add(int Hotel_No, string Name, string Address)
+    //    {
+    //        Hotels.Add(new Hotel(Hotel_No, Name, Address));
+
+    //    }
+
+    //    public void Remove(Hotel thisHotel)
+    //    {
+    //        Hotels.Remove(thisHotel);
+
+    //    }
+    }
+}
