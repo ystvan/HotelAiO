2736b885303ab894fb81fdf10e8378a0bdca169a
ystvan <istv0050@edu.easj.dk>
2016-04-06 10:33:22 +0200

    room handler

--- HotelMVVM/Handler/RoomHandler.cs
+++ HotelMVVM/Handler/RoomHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HotelMVVM.Model;
 using HotelMVVM.ViewModel;
 
 namespace HotelMVVM.Handler
@@ -14,6 +15,19 @@ namespace HotelMVVM.Handler
         public RoomHandler(RoomViewModel roomViewModel)
         {
             RoomViewModel = roomViewModel;
+        }
+
+        public void CreateRoom()
+        {
+            Room room = new Room(RoomViewModel.NewRoom.Room_No, RoomViewModel.NewRoom.Hotel_No, 
+                                    RoomViewModel.NewRoom.Room_Type, RoomViewModel.NewRoom.Room_Price);
+
+
+        }
+
+        public void DeleteRoom()
+        {
+            
         }
     }
 }
